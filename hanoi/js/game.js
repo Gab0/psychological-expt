fa7074ecae0c58ae7e61d5851e694358ef26b7be
Gabriel Araujo <gabriel_scf@hotmail.com>
2024-07-28 01:36:59 -0300

    Fix the HANOI game.

--- hanoi/js/game.js
+++ hanoi/js/game.js
@@ -12,14 +12,21 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+const W = game.config.width;
+const H = game.config.height;
+const Y = H * 0.8;
+
 let poles = [];
 let disks = [];
+let timestamps = [];
 let moveCount = 0;
 let moveText;
 let timer;
 let timerText;
 let startTime;
-let diskCount = 7;  // Default number of disks
+let diskCount = 3;  // Default number of disks
+
+const pole_pos = [W * 0.33, W * 0.5, W * 0.66];
 
 function preload() {
     this.load.image('disk1', 'disk1.png');
@@ -28,19 +35,20 @@ function preload() {
 }
 
 function create() {
-    poles = [
-        this.add.rectangle(200, 300, 20, 400, 0x6666ff),
-        this.add.rectangle(400, 300, 20, 400, 0x6666ff),
-        this.add.rectangle(600, 300, 20, 400, 0x6666ff)
-    ];
+    poles = pole_pos.map((x) => this.add.rectangle(x, Y / 1.45, 20, 500, 0x6666ff))
+
+    this.add.rectangle(W * 0.5, Y, W * 0.8, 30, 0x964b00);
 
     const diskColors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff, 0xffffff];
+
     for (let i = 0; i < diskCount; i++) {
-        const disk = this.add.rectangle(200, 500 - i * 30, 150 - i * 30, 30, diskColors[i % diskColors.length]);
-        disk.setInteractive();
-        this.input.setDraggable(disk);
+        const x = pole_pos[0];
+        const z = i + 1;
+        const disk = this.add.rectangle(x, Y - z * 30, 300 - i * 30, 30, diskColors[i % diskColors.length]);
         disks.push(disk);
     }
+
+    updateDiskDragableStates(this);
 
     this.input.on('dragstart', function (pointer, gameObject) {
         gameObject.setData('startX', gameObject.x);
@@ -60,7 +68,7 @@ function create() {
                 const topDisk = getTopDisk(poleIndex);
                 if (!topDisk || gameObject.width < topDisk.width) {
                     gameObject.x = pole.x;
-                    gameObject.y = 500 - getDisksOnPole(poleIndex) * 30;
+                    gameObject.y = Y - getDisksOnPole(poleIndex) * 30;
                     placed = true;
                     break;
                 }
@@ -71,15 +79,29 @@ function create() {
             gameObject.y = gameObject.getData('startY');
         } else {
             moveCount++;
+            timestamps.push(new Date());
             moveText.setText('Moves: ' + moveCount);
-            checkWinCondition();
+            if (checkWinCondition()) {
+                triggerWin(gameObject.scene);
+            };
+            updateDiskDragableStates(gameObject.scene);
         }
     });
 
-    moveText = this.add.text(16, 16, 'Moves: 0', { fontSize: '32px', fill: '#fff' });
-    timerText = this.add.text(16, 50, 'Time: 0', { fontSize: '32px', fill: '#fff' });
+    moveText = this.add.text(W * 0.1, 16, 'Moves: 0', { fontSize: '32px', fill: '#fff' });
+    timerText = this.add.text(W * 0.1, 50, 'Time: 0', { fontSize: '32px', fill: '#fff' });
+
     startTime = new Date();
-    timer = this.time.addEvent({ delay: 1000, callback: updateTimer, callbackScope: this, loop: true });
+    timer = this.time.addEvent({ delay: 100, callback: updateTimer, callbackScope: this, loop: true });
+}
+
+function updateDiskDragableStates(scene) {
+    const topDisks = poles.map(pole => getTopDisk(poles.indexOf(pole)));
+
+    disks.forEach(disk => {
+        disk.setInteractive();
+        scene.input.setDraggable(disk, topDisks.includes(disk));
+    });
 }
 
 function update() {}
@@ -94,22 +116,25 @@ function getDisksOnPole(poleIndex) {
 
 function checkWinCondition() {
     const thirdPoleDisks = disks.filter(disk => disk.x === poles[2].x).sort((a, b) => a.y - b.y);
-    if (thirdPoleDisks.length === diskCount && thirdPoleDisks.every((disk, index) => disk.width === (150 - index * 30))) {
-
+    if (thirdPoleDisks.length === diskCount) {
         return true;
     }
 }
 
-function triggerWin() {
-
-        this.add.text(400, 300, 'You Win!', { fontSize: '64px', fill: '#ff0000' }).setOrigin(0.5);
-        timer.paused = true;
-        this.input.enabled = false;
+function triggerWin(scene) {
+    scene.add.text(W * 0.5, H * 0.5, 'You Win!', { fontSize: '64px', fill: '#ff0000' }).setOrigin(0.5);
+    timer.paused = true;
+    scene.input.enabled = false;
+    updateDatabase();
 }
 
 function updateTimer() {
-    const elapsed = Math.floor((new Date() - startTime) / 1000);
-    timerText.setText('Time: ' + elapsed);
+    timerText.setText('Time: ' + getElapsedTime());
+}
+
+function getElapsedTime() {
+    return ((new Date() - startTime) / 1000).toFixed(2);
+}
 
     checkWinCondition();
 }
