c906b16d6200d8b87a0d43798ec9e021d923dae3
Gabriel Araujo <gabriel_scf@hotmail.com>
2024-08-22 00:55:02 -0300

    Fix the random name getter in the main page.

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ document.getElementById('set_username').addEventListener('click', function() {
   setCookie('username', username_input.value, 365);
 });
 
-document.getElementById('random_username').addEventListener('click', function() {
-  reloadNickname();
+document.getElementById('random_username').addEventListener('click', async function() {
+  await reloadNickname();
   username_input.value = nickname;
 });
 
