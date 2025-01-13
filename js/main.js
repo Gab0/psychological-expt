39043dd3e13efd2160e7ee10b9b27939672bd3ce
Gabriel Araujo <gabriel_scf@hotmail.com>
2025-01-13 12:10:12 -0300

    Improve the language selector.

--- js/main.js
+++ js/main.js
@@ -2,12 +2,23 @@
 import { nickname, reloadNickname, setCookie, getCookie } from '../psyexp_core.js';
 
 const username_input = document.getElementById('username');
-
 username_input.value = getCookie('username');
 
+let languageSelectors = document.getElementsByName('language');
+let chosenLanguage = getCookie('language');
 
-document.getElementById('set_username').addEventListener('click', function() {
+for (let radio of languageSelectors) {
+    if (radio.value === chosenLanguage) {
+        radio.checked = true;
+        break;
+    }
+  }
+
+document.getElementById('store_settings').addEventListener('click', function() {
   setCookie('username', username_input.value, 365);
+
+  const selectedLanguage = document.querySelector('input[name="language"]:checked').value;
+  setCookie('language', selectedLanguage, 365);
 });
 
 document.getElementById('random_username').addEventListener('click', async function() {
