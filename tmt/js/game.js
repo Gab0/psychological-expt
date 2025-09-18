8e62b137f47bf556e950c05cc17f906f54f396ca
Hed Cezar <h.developer.c@gmail.com>
2025-09-18 19:07:15 -0300

    Pencil effect, circle colors: right, wrong and neutral.

--- tmt/js/game.js
+++ tmt/js/game.js
@@ -12,148 +12,60 @@ import {
 
 document.title = 'Trail Making Test (TMT)';
 
-const userMetrics = {};
-
-function objectMap(obj, fn) {
-    const newObject = {};
-    Object.keys(obj).forEach((key) => {
-        newObject[key] = fn(obj[key]);
-    });
-    return newObject;
-}
-
 class TMTScene extends Phaser.Scene {
     constructor(markers, sceneName) {
         super({ key: sceneName });
-
-        this.markers = markers;
         this.circleData = [];
         this.circleRadius = 30;
-        this.minDistance = this.circleRadius * 3;
-
         this.currentCircle = 0;
-        this.lastCircle = null;
-        this.wrongCircle = null;
-
+        this.paths = [];
         this.isDrawing = false;
         this.isGameEnded = false;
         this.isPaused = true;
-
+        this.minDistance = this.circleRadius * 3;
+        this.lastCircle = null;
         this.startTime = 0;
-        this.pointer = null;
-
-        this.drawPath = [];
-        this.hasProgressedInCurrentDraw = false;
-        this.allDrawPaths = [];
+        this.markers = markers;
+        this.currentPath = [];
+        this.currentStep = 0;
+        this.lastErrorCircle = null;
     }
 
     create() {
-        const W = this.sys.game.config.width;
-
         this.graphics = this.add.graphics({ lineStyle: { width: 4, color: 0xffffff } });
+        this.timeText = this.add.text(700, 16, 'Time: 0.00', { fontSize: '32px', fill: '#ffffff' });
+        this.timeText.setOrigin(1, 0);
 
-        this.timeText = this.add.text(W - 20, 16, 'Time: 0.00', {
-            fontSize: '32px',
-            fill: '#ffffff'
-        }).setOrigin(1, 0);
-
-        for (let i = 0; i < 25; i++) {
+        for (let i = 0; i < this.markers.length; i++) {
             this.createUniqueCircle(i, this.markers[i]);
         }
 
         this.isPaused = false;
         this.startTime = this.time.now;
         userMetrics[this.scene.key] = [];
-        this.pointer = this.input.activePointer;
 
-        this.input.on('pointerdown', this.onPointerDown, this);
-        this.input.on('pointermove', this.onPointerMove, this);
-        this.input.on('pointerup', this.onPointerUp, this);
+        this.input.on('pointerdown', this.startDrawing, this);
+        this.input.on('pointermove', this.updateDrawing, this);
+        this.input.on('pointerup', this.stopDrawing, this);
     }
 
     update(time) {
-        if (this.isPaused || this.isGameEnded) return;
-
-        const elapsed = (time - this.startTime) / 1000;
-        this.timeText.setText(`Time: ${elapsed.toFixed(2)}`);
-
-        this.graphics.clear();
-        this.graphics.lineStyle(4, 0xffffff);
-
-        for (let path of this.allDrawPaths) {
-            this.drawPathOnGraphics(path);
+        if (!this.isPaused && !this.isGameEnded) {
+            let elapsed = (time - this.startTime) / 1000;
+            this.timeText.setText('Time: ' + elapsed.toFixed(2));
         }
 
-        if (this.drawPath.length > 1) {
-            this.drawPathOnGraphics(this.drawPath);
-        }
-
-        const next = this.circleData[this.currentCircle];
-        if (this.pointer.isDown && next?.circle.input?.enabled) {
-            const dx = this.pointer.x - next.circle.x;
-            const dy = this.pointer.y - next.circle.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance <= this.circleRadius) {
-                this.checkCorrectCircle(next.circle);
-                this.hasProgressedInCurrentDraw = true;
-
-                if (this.currentCircle >= this.circleData.length) {
-                    this.endGame(true);
-                }
-            }
-        }
-    }
-
-    // === Interações com mouse ===
-
-    onPointerDown(pointer) {
-        if (this.isPaused || this.isGameEnded) return;
-
-        this.isDrawing = true;
-        this.hasProgressedInCurrentDraw = false;
-
-        this.drawPath = this.lastCircle
-            ? [{ x: this.lastCircle.x, y: this.lastCircle.y }]
-            : [{ x: pointer.x, y: pointer.y }];
-    }
-
-    onPointerMove(pointer) {
         if (this.isDrawing) {
-            this.drawPath.push({ x: pointer.x, y: pointer.y });
+            this.redrawGraphics();
         }
     }
-
-    onPointerUp() {
-        if (!this.isDrawing) return;
-
-        this.isDrawing = false;
-
-        if (!this.hasProgressedInCurrentDraw) {
-            this.drawPath = this.lastCircle
-                ? [{ x: this.lastCircle.x, y: this.lastCircle.y }]
-                : [];
-        }
-
-        if (this.drawPath.length > 1) {
-            this.allDrawPaths.push(this.drawPath);
-        }
-
-        this.drawPath = [];
-    }
-
-    // === Círculos ===
 
     createUniqueCircle(index, content) {
-        const W = this.sys.game.config.width;
-        const H = this.sys.game.config.height;
         let x, y, overlap;
-
         do {
             overlap = false;
             x = Phaser.Math.Between(W * 0.1, W * 0.9);
             y = Phaser.Math.Between(H * 0.1, H * 0.9);
-
             for (let circle of this.circleData) {
                 if (Phaser.Math.Distance.Between(circle.x, circle.y, x, y) < this.minDistance) {
                     overlap = true;
@@ -166,133 +78,239 @@ class TMTScene extends Phaser.Scene {
     }
 
     createCircle(x, y, index, content) {
-        const circle = this.add.circle(x, y, this.circleRadius, 0xffffff);
-        const text = this.add.text(x, y, content, {
-            fontSize: '32px',
-            fill: '#000'
-        }).setOrigin(0.5);
-
+        let circle = this.add.circle(x, y, this.circleRadius, 0xffffff);
+        let text = this.add.text(x, y, content, { fontSize: '32px', fill: '#000' });
+        text.setOrigin(0.5, 0.5);
         circle.setInteractive();
         circle.num = index;
         circle.text = text;
-
-        circle.on('pointerdown', () => this.onCircleClick(circle), this);
-
         this.circleData.push({ circle, text, x, y, num: index });
     }
 
-    onCircleClick(circle) {
-        if (this.isPaused || this.isGameEnded) return;
+    startDrawing(pointer) {
+        if (this.isPaused || this.isGameEnded || this.isDrawing) return;
 
-        if (circle.num === this.currentCircle) {
-            this.checkCorrectCircle(circle);
-        } else {
-            if (this.wrongCircle) {
-                this.wrongCircle.setFillStyle(0xffffff);
+        const circle = this.getCircleAt(pointer.x, pointer.y);
+        if (!circle) return;
+
+        if (this.scene.key === "SceneB") {
+            const expectedNumberIndex = this.currentStep * 2;
+            if (circle.num !== expectedNumberIndex) {
+                this.markCircleRed(circle);
+                return;
             }
-            circle.setFillStyle(0xff0000);
-            this.wrongCircle = circle;
+
+            this.clearErrorCircle();
+
+            this.lastCircle = circle;
+            this.markCircleGreen(circle);
+
+        } else {
+            if (circle.num !== this.currentCircle) {
+                this.markCircleRed(circle);
+                return;
+            }
+
+            this.clearErrorCircle();
+
+            this.lastCircle = circle;
+
+            if (this.currentCircle === 0) {
+                this.markCircleGreen(circle);
+            }
         }
 
-        if (this.currentCircle >= this.circleData.length) {
-            this.endGame(true);
+        this.isDrawing = true;
+        this.currentPath = [new Phaser.Math.Vector2(pointer.x, pointer.y)];
+        this.redrawGraphics();
+    }
+
+    updateDrawing(pointer) {
+        if (!this.isDrawing) return;
+
+        this.currentPath.push(new Phaser.Math.Vector2(pointer.x, pointer.y));
+        this.redrawGraphics();
+
+        if (this.scene.key === "SceneB") {
+            const expectedNumberIndex = this.currentStep * 2;
+            const expectedLetterIndex = expectedNumberIndex + 1;
+
+            const targetCircle = this.getCircleAt(pointer.x, pointer.y);
+
+            if (
+                this.lastCircle &&
+                this.lastCircle.num === expectedNumberIndex &&
+                targetCircle &&
+                targetCircle.num === expectedLetterIndex
+            ) {
+                this.clearErrorCircle();
+
+                this.markCircleGreen(this.lastCircle);
+                this.markCircleGreen(targetCircle);
+                this.paths.push(this.currentPath);
+                this.currentPath = [];
+
+                this.lastCircle = targetCircle;
+                this.currentStep++;
+
+                this.isDrawing = false;
+
+                if (this.currentStep >= 13) {
+                    this.endGame(true);
+                }
+            }
+        } else {
+            const targetCircle = this.getCircleAt(pointer.x, pointer.y);
+            if (targetCircle && targetCircle.num === this.currentCircle + 1) {
+                this.clearErrorCircle();
+
+                this.markCircleGreen(targetCircle);
+                this.paths.push(this.currentPath);
+                this.currentPath = [];
+                this.lastCircle = targetCircle;
+                this.currentCircle++;
+
+                if (this.currentCircle >= this.circleData.length - 1) {
+                    this.endGame(true);
+                }
+            }
         }
     }
 
-    checkCorrectCircle(circle) {
+    stopDrawing(pointer) {
+        if (!this.isDrawing) return;
+
+        if (this.scene.key === "SceneB") {
+            this.currentPath = [];
+            this.isDrawing = false;
+            this.redrawGraphics();
+        } else {
+            this.isDrawing = false;
+            this.currentPath = [];
+            this.redrawGraphics();
+        }
+    }
+
+    getCircleAt(x, y) {
+        for (let data of this.circleData) {
+            const dx = data.x - x;
+            const dy = data.y - y;
+            if (Math.sqrt(dx * dx + dy * dy) < this.circleRadius) {
+                return data.circle;
+            }
+        }
+        return null;
+    }
+
+    markCircleGreen(circle) {
         circle.setFillStyle(0x00ff00);
         circle.disableInteractive();
-        this.lastCircle = circle;
-        this.currentCircle++;
-
-        if (this.wrongCircle) {
-            this.wrongCircle.setFillStyle(0xffffff);
-            this.wrongCircle = null;
-        }
-
         userMetrics[this.scene.key].push(this.time.now - this.startTime);
-
-        if (this.drawPath.length > 1) {
-            this.allDrawPaths.push(this.drawPath);
-        }
-
-        this.drawPath = [];
-        this.isDrawing = false;
     }
 
-    drawPathOnGraphics(path) {
-        this.graphics.beginPath();
-        this.graphics.moveTo(path[0].x, path[0].y);
-        for (let i = 1; i < path.length; i++) {
-            this.graphics.lineTo(path[i].x, path[i].y);
+    markCircleRed(circle) {
+        if (this.lastErrorCircle && this.lastErrorCircle !== circle) {
+            this.lastErrorCircle.setFillStyle(0xffffff);
+            this.lastErrorCircle.setInteractive();
         }
-        this.graphics.strokePath();
+
+        circle.setFillStyle(0xff0000);
+        this.lastErrorCircle = circle;
     }
 
-    // === Fim de jogo ===
+    clearErrorCircle() {
+        if (this.lastErrorCircle) {
+            this.lastErrorCircle.setFillStyle(0xffffff);
+            this.lastErrorCircle.setInteractive();
+            this.lastErrorCircle = null;
+        }
+    }
+
+    redrawGraphics() {
+        this.graphics.clear();
+
+        this.graphics.lineStyle(4, 0xffffff, 1);
+        for (let path of this.paths) {
+            for (let i = 1; i < path.length; i++) {
+                this.graphics.strokeLineShape(new Phaser.Geom.Line(
+                    path[i - 1].x, path[i - 1].y,
+                    path[i].x, path[i].y
+                ));
+            }
+        }
+
+        if (this.isDrawing && this.currentPath.length > 1) {
+            this.graphics.lineStyle(4, 0xffffff, 1);
+            for (let i = 1; i < this.currentPath.length; i++) {
+                this.graphics.strokeLineShape(new Phaser.Geom.Line(
+                    this.currentPath[i - 1].x, this.currentPath[i - 1].y,
+                    this.currentPath[i].x, this.currentPath[i].y
+                ));
+            }
+        }
+    }
 
     endGame(success) {
-        const midX = this.sys.game.config.width * 0.5;
-        const color = success ? '#ffffff' : '#ff0000';
-        const msg = success ? 'Test Completed!' : 'Test Failed!';
-
-        this.add.text(midX, 450, msg, {
-            fontSize: '32px',
-            fill: color
-        }).setOrigin(0.5);
-
-        this.circleData.forEach(({ circle }) => circle.disableInteractive());
         this.isGameEnded = true;
 
         if (success) {
+            this.add.text(M, 450, 'Test Completed!', { fontSize: '32px', fill: '#ffffff' }).setOrigin(0.5, 0.5);
             this.updateDatabase();
-            if (this.scene.key === 'SceneA') {
-                this.scene.start('SceneB');
+            if (this.scene.key === "SceneA") {
+                this.scene.start("SceneB");
             }
             this.highscores();
+        } else {
+            this.add.text(M, 450, 'Test Failed!', { fontSize: '32px', fill: '#ff0000' }).setOrigin(0.5, 0.5);
         }
+
+        this.circleData.forEach(({ circle }) => {
+            circle.disableInteractive();
+        });
     }
 
     updateDatabase() {
         const totalTimes = objectMap(userMetrics, (times) => times[times.length - 1]);
-
         const experimentPayload = {
             nodeTimes: userMetrics,
-            totalTimes,
-            combinedTimes: totalTimes['SceneA'] + totalTimes['SceneB']
+            totalTimes: totalTimes,
+            combinedTimes: totalTimes["SceneA"] + totalTimes["SceneB"]
         };
 
-        updateDatabase(experimentPayload, 'tmt');
+        updateDatabase(experimentPayload, "tmt");
     }
 
     highscores() {
         setTimeout(() => {
-            getHighscores('tmt', 'experiment_payload -> combinedTimes')
-                .then((scores) => this.displayHighscores(scores));
+            getHighscores("tmt", "experiment_payload -> combinedTimes")
+                .then((scores) => {
+                    this.displayHighscores(scores);
+                });
         }, 2000);
     }
 
     displayHighscores(scores) {
-        const W = this.sys.game.config.width;
-        const H = this.sys.game.config.height;
         let y = H * 0.23;
         let i = 1;
-
-        for (const score of scores) {
-            const v = score.experiment_payload.combinedTimes;
-            if (v === undefined) continue;
-
+        scores.map((score) => {
+            let v = score.experiment_payload.combinedTimes;
+            if (v === undefined) return;
             this.add.text(W * 0.2, y, `${i}.`, font.larger);
             this.add.text(W * 0.3, y, `${score.nickname}`, font.larger);
             this.add.text(W * 0.7, y, `${(v / 1000).toFixed(2)}s`, font.larger);
-
             i++;
             y += H * 0.05;
-        }
+        });
     }
 }
 
-// === Inicialização ===
+function objectMap(obj, fn) {
+    const newObject = {};
+    Object.keys(obj).forEach((key) => {
+        newObject[key] = fn(obj[key]);
+    });
+    return newObject;
+}
 
 const messageMap = await fetchMessages("pt-br", "tmt");
 
@@ -300,21 +318,28 @@ const briefing = new StandardBriefingScene(
     'Trail Making Test',
     [messageMap["BRIEFING_1"], messageMap["BRIEFING_2"]],
     'BriefingScene',
-    'SceneA'
+    'SceneA',
 );
 
 const numbers = Array.from({ length: 25 }, (_, i) => i + 1);
-const letters = Array.from({ length: 25 }, (_, i) => String.fromCharCode(65 + i));
-const mixed = [];
+const letters = Array.from({ length: 13 }, (_, i) => String.fromCharCode(65 + i));
 
-for (let i = 0; i < 25; i++) {
+const mixed = [];
+for (let i = 0; i < 13; i++) {
     mixed.push(numbers[i]);
     mixed.push(letters[i]);
 }
 
+const userMetrics = {};
+
 const roundA = new TMTScene(numbers, "SceneA");
-const roundB = new TMTScene(mixed.slice(0, 25), "SceneB");
+const roundB = new TMTScene(mixed, "SceneB");
 
 const config = PsyExpBaseConfig([briefing, roundA, roundB]);
 
 const game = new Phaser.Game(config);
+
+const W = game.config.width;
+const H = game.config.height;
+const M = W * 0.5;
+const Y = H * 0.8;
